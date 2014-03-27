08f85edc75e1c33ea1bc01a9ea4f05cda3bd9680
Reder Tseng <reder.tseng@gmail.com>
2014-03-27 00:46:52 -0700

    update index

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-Galleria.loadTheme('lib/galleria/themes/folio/galleria.folio.min.js');
+$(function(){
+    Galleria.loadTheme('lib/galleria/themes/folio/galleria.folio.min.js');
+    
+    Galleria.run('.galleria', {
+        flickr: 'set:72157642988346194',
+        flickrOptions: {
+            sort: 'date-posted-asc',
+            description: true
+        }
+    });
 
-// Initialize Galleria
-Galleria.run('.galleria', {
-    // The user & album. This example fetches the album "Demo" from the user "galleriajs"
-    picasa: 'useralbum:anti.suck.tw/Taiwan_20140324',
-    //picasa: 'useralbum:galleriajs/Demo'
-    picasaOptions: {
-    max: 200,
-    sort: 'date-posted-asc',
-    thumbSize: 'medium'
-    }
 });
-
